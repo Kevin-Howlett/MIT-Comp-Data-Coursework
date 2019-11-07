0401a4d84b214e370acecd40638ddc54215f52ba
Kevin-Howlett <54913419+Kevin-Howlett@users.noreply.github.com>
2019-11-07 18:26:52 -0500

    Add files via upload

---
+++ yieldAllCombos.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 21:35:47 2019
+
+@author: kevinhowlett
+"""
+
+def yieldAllCombos(items):
+    """
+        Generates all combinations of N items into two bags, whereby each 
+        item is in one or zero bags.
+
+        Yields a tuple, (bag1, bag2), where each bag is represented as a list 
+        of which item(s) are in each bag.
+    """
+    N = len(items)
+    for i in range(3**N):
+        bag1 = []
+        bag2 = []
+        for j in range(N):
+            if (i // 3**j) % 3 == 0:
+                bag1.append(items[j])
+            elif (i // 3**j) % 3 == 1:
+                bag2.append(items[j])
+        yield (bag1, bag2)
+        
+print(yieldAllCombos(['Hat','Shirt','Clock','TV']))
